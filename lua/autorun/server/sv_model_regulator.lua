0ab203268dd8f530c769e25948dbaacc826ad742
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-04-15 12:38:17 -0700

    Initial functional commit

--- lua/autorun/server/sv_model_regulator.lua
+++ lua/autorun/server/sv_model_regulator.lua
@@ -1,1 +1,18 @@
 -- Hold
+
+local defaultModel = "models/player/kleiner.mdl"
+local prohibitedModels = {}
+prohibitedModels["models/player/skeleton.mdl"] = true
+prohibitedModels["models/player/corpse1.mdl"] = true
+
+local function regulateModel( player )
+  local modelIsProhibited = prohibitedModels[player:GetModel()]
+  
+  if( modelIsProhibited ) then
+    player:ChatPrint( "Your selected model is prohibited, defaulting to "..defaultModel )
+    player:SetModel( defaultModel )
+  end
+end
+
+hook.Remove("PlayerSetModel", "cfc_player_model_regulator")
+hook.Add("PlayerSetModel", "cfc_player_model_regulator", regulateModel)
