d9b1b5cea927669754f85cf75b6b2ca169b61692
Justin Muskopf <birmesepython@gmail.com>
2018-07-01 20:49:54 -0500

    prohibitedModels -> modelIsProhibited

--- lua/autorun/server/sv_model_regulator.lua
+++ lua/autorun/server/sv_model_regulator.lua
@@ -1,16 +1,16 @@
 local defaultModel = "models/player/kleiner.mdl"
 
-local prohibitedModels = {}
-prohibitedModels["models/player/skeleton.mdl"] = true
-prohibitedModels["models/player/corpse1.mdl"]  = true
-prohibitedModels["models/player/charple.mdl"]  = true
+local modelIsProhibited = {}
+modelIsProhibited["models/player/skeleton.mdl"] = true
+modelIsProhibited["models/player/corpse1.mdl"]  = true
+modelIsProhibited["models/player/charple.mdl"]  = true
 
 local playerMeta = FindMetaTable("Player")
 local entityMeta = FindMetaTable("Entity")
 local setModel = entityMeta.SetModel
 
 function playerMeta:SetModel(desiredModel)
-	local modelIsProhibited = prohibitedModels[desiredModel]
+	local modelIsProhibited = modelIsProhibited[desiredModel]
 	
 	if( modelIsProhibited ) then
 		return setModel(self, defaultModel)
