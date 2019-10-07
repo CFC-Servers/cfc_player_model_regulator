e19b43fff5db2109728d663afef4660a56b5a988
Brandon Sturgeon <sturgeonb4@gmail.com>
2019-10-07 18:01:16 -0400

    Various whitespace fixes

--- lua/autorun/server/sv_model_regulator.lua
+++ lua/autorun/server/sv_model_regulator.lua
@@ -4,12 +4,12 @@ local modelIsProhibited = {}
 modelIsProhibited["models/player/skeleton.mdl"] = true
 modelIsProhibited["models/player/charple.mdl"]  = true
 
-local playerMeta = FindMetaTable("Player")
-local entityMeta = FindMetaTable("Entity")
+local playerMeta = FindMetaTable( "Player" )
+local entityMeta = FindMetaTable( "Entity" )
 local setModel = entityMeta.SetModel
 
-function playerMeta:SetModel(desiredModel)
-	local model = ( modelIsProhibited[desiredModel] and defaultModel ) or desiredModel
+function playerMeta:SetModel( desiredModel )
+    local model = ( modelIsProhibited[desiredModel] and defaultModel ) or desiredModel
 
-	return setModel(self, model)
+    return setModel( self, model )
 end
