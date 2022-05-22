4f414f4402b17b6085d36cca20dc5025f862d2c3
legokidlogan <legokidlogan@gmail.com>
2022-05-22 13:10:26 -0700

    Create tests for SetModel

---
+++ lua/tests/cfc_restrict_playermodels/main.lua
@@ -0,0 +1,40 @@
+local defaultModel = "models/player/kleiner.mdl"
+local badModel = "models/player/skeleton.mdl"
+local goodModel = "models/player/group01/male_07.mdl"
+
+local function testSetModel( requestedModel )
+    local finalModel = ""
+
+    local playerMeta = FindMetaTable( "Player" )
+    local entityMeta = FindMetaTable( "Entity" )
+
+    local ogSetModel = entityMeta.SetModel
+
+    entityMeta.SetModel = function( self, mdl )
+        finalModel = mdl
+    end
+
+    playerMeta.SetModel( {}, requestedModel ) -- Apply subject function
+    entityMeta.SetModel = ogSetModel -- Revert
+
+    return finalModel
+end
+
+return {
+    {
+        name = "It should replace undesireable playermodels with the default.",
+        func = function()
+            finalModel = testSetModel( badModel )
+
+            expect( finalModel ).to.eq( defaultModel )
+        end
+    },
+    {
+        name = "It should leave good playermodels unchanged.",
+        func = function()
+            finalModel = testSetModel( goodModel )
+
+            expect( finalModel ).to.eq( goodModel )
+        end
+    },
+}
