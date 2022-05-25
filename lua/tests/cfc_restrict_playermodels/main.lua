3a007d20e649af7742e90a4905b64048b52d6645
legokidlogan <legokidlogan@gmail.com>
2022-05-24 18:29:06 -0700

    Debug GLT

--- lua/tests/cfc_restrict_playermodels/main.lua
+++ lua/tests/cfc_restrict_playermodels/main.lua
@@ -42,12 +42,6 @@ return {
 
                 expect( finalModel ).to.eq( goodModel )
             end
-        },
-        {
-            name = "This is a failure",
-            func = function( state )
-                expect( false ).to.beTrue()
-            end
         }
     }
 }
